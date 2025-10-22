8349aeccccb6ff58c1915215ef42c77dec746dc3
scubesoftsolutions <project@scubesoftsolutions.com>
2025-10-21 21:38:45 -0400

    Update settings.py

--- atms/settings.py
+++ atms/settings.py
@@ -60,10 +60,11 @@ ROOT_URLCONF = 'atms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'main' / 'Templates'],  # capital T here since your folder is 'Templates'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -71,6 +72,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'atms.wsgi.application'
 
